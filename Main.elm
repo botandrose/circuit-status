aea91b50002de2c27a6655c15153bfc847bfddfb
Micah Geisel <micah@botandrose.com>
2016-02-20 23:45:38 -0800

    get most basic awful prototype working.

--- Main.elm
+++ Main.elm
@@ -1,7 +1,39 @@
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import StartApp.Simple as StartApp
 
 
-main : Html.Html
+type Status = Open | Closed
+
+
+type Action = Noop
+
+
+model = [ Open, Closed, Open, Closed ]
+
+
+view address model =
+  let
+    sectionView status =
+      let
+        color =
+          case status of
+            Open -> "green"
+            Closed -> "red"
+
+      in 
+        div
+          [ style [( "background-color", color )] ]
+          [ text "Section" ]
+
+  in
+    div [] (List.map sectionView model)
+
+
+update action model =
+  model
+
+
 main =
-  Html.text "hello world!"
+  StartApp.start { model = model, view = view, update = update }
 
