dcef3fbe0bb043dca56a0a42fc8df8cc08a1ba5e
Micah Geisel <micah@botandrose.com>
2016-02-21 02:14:19 -0800

    cargo cult some stuff and bash head into desk until it works.

--- src/CircuitStatus.elm
+++ src/CircuitStatus.elm
@@ -1,26 +1,95 @@
+import Dict
 import Effects exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import StartApp
 import Task exposing (Task)
 
+import Json.Decode as JD exposing ((:=))
+import Json.Encode as JE
+import Signal exposing (Mailbox, Address, mailbox, message)
+import Task exposing (Task, andThen)
+import Effects exposing (Effects, Never)
+import StartApp
+
+import ElmFire
+import ElmFire.Dict
+
 
 type Status = Open | Closed
 
 
-type Action = Noop
+statusToBool : Status -> Bool
+statusToBool status =
+  case status of
+    Open -> True
+    Closed -> False
+
+
+boolToStatus : Bool -> Status
+boolToStatus bool =
+  case bool of
+    True -> Open
+    False -> Closed
+
+
+type Action = Noop | FromServer (Dict.Dict String Model)
+
 
 
 type alias Model = List Status
 
 
+firebaseUrl : String
+firebaseUrl = "https://shining-inferno-6056.firebaseio.com"
+
+
+-- Mirror Firebase's content as the model's items
+
+-- initialTask : Task Error (Task Error ())
+-- inputItems : Signal ???
+(initialTask, inputItems) =
+  ElmFire.Dict.mirror syncConfig
+
+
+--------------------------------------------------------------------------------
+
+syncConfig : ElmFire.Dict.Config Model
+syncConfig =
+  { location = ElmFire.fromUrl firebaseUrl
+  , orderOptions = ElmFire.noOrder
+  , encoder =
+    \model ->
+      JE.object [ ("sections", JE.list (List.map (\status -> JE.bool (statusToBool status)) model)) ]
+  , decoder =
+    ( "sections" := (JD.list (JD.bool `JD.andThen` (\bool -> JD.succeed (boolToStatus bool)))) )
+  }
+
+--------------------------------------------------------------------------------
+
 initialModel : Model
-initialModel = [ Open, Closed, Open, Closed ]
+initialModel = []
+
+
+initialEffect : Effects Action
+initialEffect = initialTask |> kickOff
+
+--------------------------------------------------------------------------------
+
+-- Map any task to an effect, discarding any direct result or error value
+kickOff : Task x a -> Effects Action
+kickOff =
+  Task.toMaybe >> Task.map (always (Noop)) >> Effects.task
 
 
 update : Action -> Model -> ( Model, Effects Action )
 update action model =
-  ( model, Effects.none )
+  case action of
+    Noop ->
+      ( model, Effects.none )
+
+    FromServer dict ->
+      ( Maybe.withDefault [] (Dict.get "sections" dict), Effects.none )
 
 
 view : Signal.Address Action -> Model -> Html
@@ -33,7 +102,7 @@ view address model =
             Open -> "green"
             Closed -> "red"
 
-      in 
+      in
         div
           [ style [( "background-color", color )] ]
           [ text "Section" ]
@@ -44,10 +113,10 @@ view address model =
 
 config : StartApp.Config Model Action
 config =
-  { init = (initialModel, Effects.none)
+  { init = ( initialModel, initialEffect )
   , update = update
   , view = view
-  , inputs = [ Signal.constant Noop ]
+  , inputs = [ Signal.map FromServer inputItems ]
   }
 
 
