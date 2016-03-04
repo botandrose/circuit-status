1f1cfb257ab09d5a18c9843d88c4524a9872042b
Michael Gubitosa <gubs@botandrose.com>
2016-03-03 16:02:05 -0800

    Add remaining routes.

--- src/Model.elm
+++ src/Model.elm
@@ -9,12 +9,12 @@ type Action = Noop | FromServer (List Section) | ToggleSection SectionId | Toggl
 type Status = Open | Closed
 
 
-type SectionId = CaveLeft | CaveRight | BoneBreaker | AlleyCave | AlleyNorthwest | AlleyNortheast | Entrance | Overhang | Staircase
+type SectionId = CaveLeft | CaveRight | BoneBreaker | AlleyCave | AlleyNorthwest | AlleyNortheast | Entrance | Overhang | Staircase | Bottom1 | Bottom2 | Bottom3 | Bottom4 | Bottom5 | Bottom6 | Top1 | Top2 | Top3 | Top4 | Top5 | Right1 | Right2 | Right3 | Right4 | Right5 | Right6 | Right7 | Right8 | Right9
 
 
 sectionIds : List SectionId
 sectionIds =
-  [ CaveLeft, CaveRight, BoneBreaker, AlleyCave, AlleyNorthwest, AlleyNortheast, Entrance, Overhang, Staircase ]
+  [ CaveLeft, CaveRight, BoneBreaker, AlleyCave, AlleyNorthwest, AlleyNortheast, Entrance, Overhang, Staircase, Bottom1, Bottom2, Bottom3, Bottom4, Bottom5, Bottom6, Top1, Top2, Top3, Top4, Top5, Right1, Right2, Right3, Right4, Right5, Right6, Right7, Right8, Right9 ]
 
 
 type alias Section = { sectionId : SectionId, status : Status, updatedAt : Time }
